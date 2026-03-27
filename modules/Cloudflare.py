d3905ae1153078bb39b1342922a94dd9e09c2d98
dudu7615 <dudu7615@qq.com>
2026-03-27 19:29:29 +0800

    feat(cloudflare): auto exit thread

--- modules/Cloudflare.py
+++ modules/Cloudflare.py
@@ -6,18 +6,26 @@ import io
 from queue import Queue
 from modules import Paths, logger
 
+
+_exitReadOutput = False
+
 def _read_output(stream: io.TextIOWrapper | None, queue: Queue[str]):
     """在独立线程中读取流数据"""
+    global _exitReadOutput
+
     if stream is None:
         return
     for line in stream:
         queue.put(line)
+        if _exitReadOutput:
+            return
 
 def run(port: int, timeout: int = 10) -> str | None:
     """
     启动 cloudflared 进程并在 {timeout} 秒内从其输出中提取 URL
     进程会在后台持续运行
     """
+    global _exitReadOutput
     # cloudflared tunnel --url http://127.0.0.1:8000
     process = subprocess.Popen(
         [str(Paths.bin / "cloudflared.exe"), "tunnel", "--url", f"http://127.0.0.1:{port}"],
@@ -28,8 +36,8 @@ def run(port: int, timeout: int = 10) -> str | None:
     
     # 创建队列和线程来读取输出
     queue: Queue[str] = Queue()
-    stdoutThread = threading.Thread(target=_read_output, args=(process.stdout, queue), daemon=True)
-    stderrThread = threading.Thread(target=_read_output, args=(process.stderr, queue), daemon=True)
+    stdoutThread = threading.Thread(name = "CloudflareStdout", target=_read_output, args=(process.stdout, queue), daemon=True)
+    stderrThread = threading.Thread(name = "CloudflareStderr", target=_read_output, args=(process.stderr, queue), daemon=True)
     stdoutThread.start()
     stderrThread.start()
     
@@ -44,15 +52,18 @@ def run(port: int, timeout: int = 10) -> str | None:
                 if match:
                     url = match.group(0)
                     logger.info(f"Cloudflare URL: {url}")
+                    _exitReadOutput = True
                     return url
             except:
                 # 队列为空，继续等待
                 pass
         
         logger.error(f"在 {timeout} 秒内未能提取 Cloudflare URL")
+        _exitReadOutput = True
         return None
     except Exception as e:
         logger.error(f"获取 Cloudflare URL 时出错: {e}")
+        _exitReadOutput = True
         return None
 
 
