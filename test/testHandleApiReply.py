ad7e465d6adbb86cd2c4382feadeb63e9a17ef66
dudu <dudu7615@qq.com>
2026-04-24 19:50:28 +0800

    test(sql data): 数据解析测试

--- test/testHandleApiReply.py
+++ test/testHandleApiReply.py
@@ -1,8 +1,13 @@
 from modules import DataTypes
 from pathlib import Path
+from modules import Sql
+
 
 resultJson = Path("test/1/reply.json").read_text(encoding="utf-8")
 
 test: DataTypes.ApiReply = DataTypes.ApiReply.model_validate_json(resultJson)
 
-print(test)
+Sql.Papers.mark(1, comment=test)
+
+if paper := Sql.Papers.get(1):
+    print(paper.comment)
